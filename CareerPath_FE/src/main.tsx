05c6c05fc464e35127a1424b61faad5c47b6315a
phuclee <phucle682004@gmail.com>
2026-06-01 11:34:39 +0700

    update env

--- CareerPath_FE/src/main.tsx
+++ CareerPath_FE/src/main.tsx
@@ -7,6 +7,9 @@ import './index.css';
 // Client ID is now loaded from .env file
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
+import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
